58c8241f9a38b4e7995adb9db8345f665c2be3d4
jeremie <jeremieroland@orange.fr>
2019-10-05 19:26:53 +0200

    Logique états/transitions faite en partie

--- src/app/Logic/Model.fs
+++ src/app/Logic/Model.fs
@@ -4,23 +4,31 @@ open System
 
 // Then our commands
 type Command =
-    | RequestTimeOff of TimeOffRequest
+    | RequestTimeOff  of TimeOffRequest
     | ValidateRequest of UserId * Guid
+    | CancelRequest   of UserId * Guid
+    | RejectRequest   of UserId * Guid
     with
     member this.UserId =
         match this with
         | RequestTimeOff request -> request.UserId
-        | ValidateRequest (userId, _) -> userId
+        | ValidateRequest (userId, _)
+        | CancelRequest   (userId, _)
+        | RejectRequest   (userId, _) -> userId
 
 // And our events
 type RequestEvent =
-    | RequestCreated of TimeOffRequest
+    | RequestCreated   of TimeOffRequest
     | RequestValidated of TimeOffRequest
+    | RequestCanceled  of TimeOffRequest
+    | RequestRejected  of TimeOffRequest
     with
     member this.Request =
         match this with
-        | RequestCreated request -> request
-        | RequestValidated request -> request
+        | RequestCreated   request
+        | RequestValidated request
+        | RequestRejected  request
+        | RequestCanceled  request -> request
 
 // We then define the state of the system,
 // and our 2 main functions `decide` and `evolve`
@@ -29,29 +37,66 @@ module Logic =
     type RequestState =
         | NotCreated
         | PendingValidation of TimeOffRequest
-        | Validated of TimeOffRequest with
+        | Validated of TimeOffRequest
+        | Canceled of TimeOffRequest
+        | Rejected of TimeOffRequest
+        | RejectedCancellation of TimeOffRequest
+        | PendingCancellation of TimeOffRequest with
         member this.Request =
             match this with
             | NotCreated -> invalidOp "Not created"
             | PendingValidation request
-            | Validated request -> request
+            | Validated request
+            | Canceled request
+            | Rejected request
+            | RejectedCancellation request
+            | PendingCancellation request -> request
         member this.IsActive =
             match this with
-            | NotCreated -> false
-            | PendingValidation _
-            | Validated _ -> true
+            | NotCreated
+            | Canceled _
+            | Rejected _ -> false
+            | _ -> true
 
     type UserRequestsState = Map<Guid, RequestState>
 
-    let evolveRequest state event =
+    // Pour cette fonction, voir le schéma états/transitions
+    let evolveRequest (state: RequestState) (event: RequestEvent) =
         match event with
-        | RequestCreated request -> PendingValidation request
-        | RequestValidated request -> Validated request
+
+        | RequestCreated request -> 
+            match state with
+            | NotCreated -> Ok [PendingValidation request]
+            | _ -> Error "Cannot create an already existing request."
+
+        | RequestValidated request ->
+            match state with
+            | PendingValidation _ -> Ok [Validated request]
+            | _ -> Error "Cannot validate a request not in pending validation."
+
+        | RequestCanceled request ->
+            match state with
+            | PendingValidation _
+            | Validated _
+            | RejectedCancellation _
+            | PendingCancellation _ -> Ok [Canceled request]
+            | _ -> Error "Cannot cancel."
+        
+        | RequestRejected request ->
+            match state with
+            | PendingValidation _ -> Ok [Rejected request]
+            | _ -> Error "Cannot reject a request not in pending validation."
+
 
     let evolveUserRequests (userRequests: UserRequestsState) (event: RequestEvent) =
         let requestState = defaultArg (Map.tryFind event.Request.RequestId userRequests) NotCreated
         let newRequestState = evolveRequest requestState event
-        userRequests.Add (event.Request.RequestId, newRequestState)
+        match newRequestState with
+            | Ok stateList -> userRequests.Add (event.Request.RequestId, stateList.Head)
+            | _ -> userRequests
+
+
+
 
     let overlapsWith (request1: TimeOffRequest) (request2: TimeOffRequest) =
         not(
@@ -67,6 +112,9 @@ module Logic =
         else (Seq.head otherRequests) |> overlapsWith request
             || overlapsWithAnyRequest (Seq.tail otherRequests) request
 
+
+
+
     let createRequest activeUserRequests  request (today: DateTime) =
         if request |> overlapsWithAnyRequest activeUserRequests then
             Error "Overlapping request"
@@ -77,10 +125,31 @@ module Logic =
 
     let validateRequest requestState =
         match requestState with
-        | PendingValidation request ->
-            Ok [RequestValidated request]
-        | _ ->
-            Error "Request cannot be validated"
+        | PendingValidation request -> Ok [RequestValidated request]
+        | _ -> Error "Request cannot be validated"
+    
+    let cancelRequest requestState user =
+        match user with
+        | Manager ->
+            match requestState with
+            | PendingValidation request
+            | PendingCancellation request
+            | Validated request
+            | RejectedCancellation request -> Ok [RequestCanceled request]
+            | _ -> Error "Request cannot be canceled"
+        | Employee _ ->
+            match requestState with
+            | Validated request
+            | PendingValidation request -> Ok[RequestCanceled request]
+            | _ -> Error "Request cannot be canceled"
+    
+    let rejectRequest requestState =
+        match requestState with
+        | PendingValidation request -> Ok [RequestRejected request]
+        | _ -> Error "Request cannot be rejected"
+
+
+
 
     let decide (userRequests: UserRequestsState) (user: User) (command: Command) (today: DateTime) =
         let relatedUserId = command.UserId
@@ -105,3 +174,14 @@ module Logic =
                 else
                     let requestState = defaultArg (userRequests.TryFind requestId) NotCreated
                     validateRequest requestState
+
+            | CancelRequest (_, requestId) ->
+                let requestState = defaultArg (userRequests.TryFind requestId) NotCreated
+                cancelRequest requestState user
+
+            | RejectRequest (_, requestId) ->
+                if user <> Manager then
+                    Error "Unauthorized"
+                else
+                    let requestState = defaultArg (userRequests.TryFind requestId) NotCreated
+                    rejectRequest requestState
--- src/test/Logic.Tests/Tests.fs
+++ src/test/Logic.Tests/Tests.fs
@@ -153,4 +153,127 @@ let validationTests =
       |> When (ValidateRequest ("jdoe", request.RequestId))
       |> Then (Ok [RequestValidated request]) "The request should have been validated"
     }
+
+    test "A request cannot be validated by oneself" {
+      let request = {
+        UserId = "jdoe"
+        RequestId = Guid.NewGuid()
+        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
+        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
+
+      Given [ RequestCreated request ]
+      |> ConnectedAs (Employee "jdoe")
+      |> WithToday (DateTime(2019, 1, 1))
+      |> When (ValidateRequest ("jdoe", request.RequestId))
+      |> Then (Error "Unauthorized") "The request should not have been validated by oneself."
+    }
+
+    test "A validated request cannot be validated" {
+      let request = {
+        UserId = "jdoe"
+        RequestId = Guid.NewGuid()
+        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
+        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
+
+      Given [ RequestCreated request ; RequestValidated request ]
+      |> ConnectedAs Manager
+      |> WithToday (DateTime(2019, 1, 1))
+      |> When (ValidateRequest ("jdoe", request.RequestId))
+      |> Then (Error "Request cannot be validated") "The validated request should not have been validated."
+    }
+  ]
+
+
+[<Tests>]
+let rejectionTests =
+  testList "Rejection tests" [
+    test "A request is rejected" {
+      let request = {
+        UserId = "jdoe"
+        RequestId = Guid.NewGuid()
+        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
+        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
+
+      Given [ RequestCreated request ]
+      |> ConnectedAs Manager
+      |> WithToday (DateTime(2019, 1, 1))
+      |> When (RejectRequest ("jdoe", request.RequestId))
+      |> Then (Ok [RequestRejected request]) "The request should have been rejected."
+    }
+
+    test "A request cannot be rejected by oneself" {
+      let request = {
+        UserId = "jdoe"
+        RequestId = Guid.NewGuid()
+        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
+        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
+
+      Given [ RequestCreated request ]
+      |> ConnectedAs (Employee "jdoe")
+      |> WithToday (DateTime(2019, 1, 1))
+      |> When (RejectRequest ("jdoe", request.RequestId))
+      |> Then (Error "Unauthorized") "The request should not have been rejected by oneself."
+    }
+
+    test "A validated request cannot be rejected" {
+      let request = {
+        UserId = "jdoe"
+        RequestId = Guid.NewGuid()
+        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
+        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
+
+      Given [ RequestCreated request ; RequestValidated request ]
+      |> ConnectedAs Manager
+      |> WithToday (DateTime(2019, 1, 1))
+      |> When (RejectRequest ("jdoe", request.RequestId))
+      |> Then (Error "Request cannot be rejected") "The validated request should not have been rejected."
+    }
+  ]
+
+
+
+[<Tests>]
+let CancellationTests =
+  testList "Cancellation tests" [
+    test "A request is canceled by the manager" {
+      let request = {
+        UserId = "jdoe"
+        RequestId = Guid.NewGuid()
+        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
+        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
+
+      Given [ RequestCreated request ]
+      |> ConnectedAs Manager
+      |> WithToday (DateTime(2019, 1, 1))
+      |> When (CancelRequest ("jdoe", request.RequestId))
+      |> Then (Ok [RequestCanceled request]) "The request should have been canceled by the manager."
+    }
+
+    test "A request is canceled by the employee" {
+      let request = {
+        UserId = "jdoe"
+        RequestId = Guid.NewGuid()
+        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
+        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
+
+      Given [ RequestCreated request ]
+      |> ConnectedAs (Employee "jdoe")
+      |> WithToday (DateTime(2019, 1, 1))
+      |> When (CancelRequest ("jdoe", request.RequestId))
+      |> Then (Ok [RequestCanceled request]) "The request should have been canceled by the employee."
+    }
+
+    test "A rejected request cannot be canceled" {
+      let request = {
+        UserId = "jdoe"
+        RequestId = Guid.NewGuid()
+        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
+        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
+
+      Given [ RequestCreated request ; RequestRejected request]
+      |> ConnectedAs Manager
+      |> WithToday (DateTime(2019, 1, 1))
+      |> When (CancelRequest ("jdoe", request.RequestId))
+      |> Then (Error "Request cannot be canceled") "The request should have been canceled by the employee."
+    }
   ]
