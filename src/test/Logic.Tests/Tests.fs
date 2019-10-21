e6cebc03d1d6a71e1aa4defd7a0d97764a5815e6
jeremie <jeremieroland@orange.fr>
2019-10-21 18:51:08 +0200

    refacto tests

--- src/test/Logic.Tests/Tests.fs
+++ src/test/Logic.Tests/Tests.fs
@@ -20,338 +20,206 @@ let Then expected message (events: RequestEvent list, user: User, today: DateTim
 
 open System
 
+let reqOctober1 = {
+  UserId = "jdoe"
+  RequestId = Guid.NewGuid()
+  Start = { Date = DateTime(2019, 10, 1); HalfDay = AM }
+  End = { Date = DateTime(2019, 10, 1); HalfDay = PM }
+}
+
+let reqOctober2 = {
+  UserId = "jdoe"
+  RequestId = Guid.NewGuid()
+  Start = { Date = DateTime(2019, 10, 2); HalfDay = AM }
+  End = { Date = DateTime(2019, 10, 2); HalfDay = PM }
+}
+
+let reqOctober3 = {
+  UserId = "jdoe"
+  RequestId = Guid.NewGuid()
+  Start = { Date = DateTime(2019, 10, 3); HalfDay = AM }
+  End = { Date = DateTime(2019, 10, 3); HalfDay = PM }
+}
+
+let reqJanuaryFull = {
+  UserId = "jdoe"
+  RequestId = Guid.NewGuid()
+  Start = { Date = DateTime(2019, 1, 1); HalfDay = AM }
+  End = { Date = DateTime(2019, 1, 31); HalfDay = PM }
+}
+
+let reqMidJanuaryToMidFebruary = {
+  UserId = "jdoe"
+  RequestId = Guid.NewGuid()
+  Start = { Date = DateTime(2019, 1, 15); HalfDay = AM }
+  End = { Date = DateTime(2019, 2, 15); HalfDay = PM }
+}
+
 [<Tests>]
 let overlapTests = 
   testList "Overlap tests" [
     test "A request overlaps with itself" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 10, 1); HalfDay = AM }
-        End = { Date = DateTime(2019, 10, 1); HalfDay = PM }
-      }
-
-      Expect.isTrue (Logic.overlapsWith request request) "A request should overlap with istself"
+      Expect.isTrue (Logic.overlapsWith reqOctober1 reqOctober1) "A request should overlap with istself"
     }
 
     test "2 requests overlap" {
-      let request1 = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 1, 1); HalfDay = AM }
-        End = { Date = DateTime(2019, 2, 1); HalfDay = PM }
-      }
-      let request2 = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 1, 15); HalfDay = AM }
-        End = { Date = DateTime(2019, 2, 15); HalfDay = PM }
-      }
-
-      Expect.isTrue (Logic.overlapsWith request1 request2 && Logic.overlapsWith request1 request2) "2 requests should overlap, even with reversed order"
+      Expect.isTrue (Logic.overlapsWith reqJanuaryFull reqMidJanuaryToMidFebruary && Logic.overlapsWith reqMidJanuaryToMidFebruary reqJanuaryFull) "2 requests should overlap, even with reversed order"
     }
 
     test "Requests on 2 distinct days don't overlap" {
-      let request1 = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 10, 1); HalfDay = AM }
-        End = { Date = DateTime(2019, 10, 1); HalfDay = PM }
-      }
-
-      let request2 = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 10, 2); HalfDay = AM }
-        End = { Date = DateTime(2019, 10, 2); HalfDay = PM }
-      }
-
-      Expect.isFalse (Logic.overlapsWith request1 request2) "The requests don't overlap"
+      Expect.isFalse (Logic.overlapsWith reqOctober1 reqOctober2) "The requests don't overlap"
     }
 
     test "Requests on 3 distinct days don't overlap" {
       let otherRequests = seq {
-        yield {
-          UserId = "jdoe"
-          RequestId = Guid.NewGuid()
-          Start = { Date = DateTime(2019, 10, 1); HalfDay = AM }
-          End = { Date = DateTime(2019, 10, 1); HalfDay = PM }
-        }
-        yield {
-          UserId = "jdoe"
-          RequestId = Guid.NewGuid()
-          Start = { Date = DateTime(2019, 10, 2); HalfDay = AM }
-          End = { Date = DateTime(2019, 10, 2); HalfDay = PM }
-        }
+        yield reqOctober1
+        yield reqOctober2
       }
-
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 10, 3); HalfDay = AM }
-        End = { Date = DateTime(2019, 10, 3); HalfDay = PM }
-      }
-
-      Expect.isFalse (Logic.overlapsWithAnyRequest otherRequests request) "The requests don't overlap"
+      Expect.isFalse (Logic.overlapsWithAnyRequest otherRequests reqOctober3) "The requests don't overlap"
     }
 
     test "A request overlaps with at least one of anothers" {
       let otherRequests = seq {
-        yield {
-          UserId = "jdoe"
-          RequestId = Guid.NewGuid()
-          Start = { Date = DateTime(2019, 10, 1); HalfDay = AM }
-          End = { Date = DateTime(2019, 10, 1); HalfDay = PM }
-        }
-        yield {
-          UserId = "jdoe"
-          RequestId = Guid.NewGuid()
-          Start = { Date = DateTime(2019, 10, 2); HalfDay = AM }
-          End = { Date = DateTime(2019, 10, 2); HalfDay = PM }
-        }
+        yield reqOctober1
+        yield reqOctober2
       }
-
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 10, 2); HalfDay = AM }
-        End = { Date = DateTime(2019, 10, 2); HalfDay = PM }
-      }
-
-      Expect.isTrue (Logic.overlapsWithAnyRequest otherRequests request) "A request should overlap with another with the same date"
+      Expect.isTrue (Logic.overlapsWithAnyRequest otherRequests reqOctober2) "A request should overlap with another with the same date"
     }
   ]
-
-
 
 
 [<Tests>]
 let creationTests =
   testList "Creation tests" [
     test "A request is created" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
       Given [ ]
       |> ConnectedAs (Employee "jdoe")
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (RequestTimeOff request)
-      |> Then (Ok [RequestCreated request]) "The request should have been created"
+      |> When (RequestTimeOff reqOctober1)
+      |> Then (Ok [RequestCreated reqOctober1]) "The request should have been created"
     }
 
-    test "A request is created in the past" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
+    test "A request cannot be created in the past" {
       Given [ ]
       |> ConnectedAs (Employee "jdoe")
       |> WithToday (DateTime(2019, 12, 31))
-      |> When (RequestTimeOff request)
+      |> When (RequestTimeOff reqOctober1)
       |> Then (Error "The request starts in the past") "The request should not have been created because it is in the past!"
     }
   ]
-
-
-
 
 
 [<Tests>]
 let validationTests =
   testList "Validation tests" [
     test "A request is validated" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
-      Given [ RequestCreated request ]
+      Given [ RequestCreated reqOctober1 ]
       |> ConnectedAs Manager
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (ValidateRequest ("jdoe", request.RequestId))
-      |> Then (Ok [RequestValidated request]) "The request should have been validated"
+      |> When (ValidateRequest ("jdoe", reqOctober1.RequestId))
+      |> Then (Ok [RequestValidated reqOctober1]) "The request should have been validated"
     }
 
     test "A request cannot be validated by oneself" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
-      Given [ RequestCreated request ]
+      Given [ RequestCreated reqOctober1 ]
       |> ConnectedAs (Employee "jdoe")
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (ValidateRequest ("jdoe", request.RequestId))
+      |> When (ValidateRequest ("jdoe", reqOctober1.RequestId))
       |> Then (Error "Unauthorized") "The request should not have been validated by oneself."
     }
 
     test "A validated request cannot be validated" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
-      Given [ RequestCreated request ; RequestValidated request ]
+      Given [ RequestCreated reqOctober1 ; RequestValidated reqOctober1 ]
       |> ConnectedAs Manager
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (ValidateRequest ("jdoe", request.RequestId))
+      |> When (ValidateRequest ("jdoe", reqOctober1.RequestId))
       |> Then (Error "Request cannot be validated") "The validated request should not have been validated."
     }
   ]
-
-
 
 
 [<Tests>]
 let rejectionTests =
   testList "Rejection tests" [
     test "A request is rejected" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
-      Given [ RequestCreated request ]
+      Given [ RequestCreated reqOctober1 ]
       |> ConnectedAs Manager
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (RejectRequest ("jdoe", request.RequestId))
-      |> Then (Ok [RequestRejected request]) "The request should have been rejected."
+      |> When (RejectRequest ("jdoe", reqOctober1.RequestId))
+      |> Then (Ok [RequestRejected reqOctober1]) "The request should have been rejected."
     }
 
     test "A request cannot be rejected by oneself" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
-      Given [ RequestCreated request ]
+      Given [ RequestCreated reqOctober1 ]
       |> ConnectedAs (Employee "jdoe")
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (RejectRequest ("jdoe", request.RequestId))
+      |> When (RejectRequest ("jdoe", reqOctober1.RequestId))
       |> Then (Error "Unauthorized") "The request should not have been rejected by oneself."
     }
 
     test "A validated request cannot be rejected" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
-      Given [ RequestCreated request ; RequestValidated request ]
+      Given [ RequestCreated reqOctober1 ; RequestValidated reqOctober1 ]
       |> ConnectedAs Manager
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (RejectRequest ("jdoe", request.RequestId))
+      |> When (RejectRequest ("jdoe", reqOctober1.RequestId))
       |> Then (Error "Request cannot be rejected") "The validated request should not have been rejected."
     }
   ]
-
-
-
 
 
 [<Tests>]
 let cancellationTests =
   testList "Cancellation tests" [
     test "A request is canceled by the manager" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
-      Given [ RequestCreated request ]
+      Given [ RequestCreated reqOctober1 ]
       |> ConnectedAs Manager
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (CancelRequest ("jdoe", request.RequestId))
-      |> Then (Ok [RequestCanceled request]) "The request should have been canceled by the manager."
+      |> When (CancelRequest ("jdoe", reqOctober1.RequestId))
+      |> Then (Ok [RequestCanceled reqOctober1]) "The request should have been canceled by the manager."
     }
 
     test "A request is canceled by the employee" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
-      Given [ RequestCreated request ]
+      Given [ RequestCreated reqOctober1 ]
       |> ConnectedAs (Employee "jdoe")
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (CancelRequest ("jdoe", request.RequestId))
-      |> Then (Ok [RequestCanceled request]) "The request should have been canceled by the employee."
+      |> When (CancelRequest ("jdoe", reqOctober1.RequestId))
+      |> Then (Ok [RequestCanceled reqOctober1]) "The request should have been canceled by the employee."
     }
 
     test "A rejected request cannot be canceled" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 12, 27); HalfDay = AM }
-        End = { Date = DateTime(2019, 12, 27); HalfDay = PM } }
-
-      Given [ RequestCreated request ; RequestRejected request]
+      Given [ RequestCreated reqOctober1 ; RequestRejected reqOctober1]
       |> ConnectedAs Manager
       |> WithToday (DateTime(2019, 1, 1))
-      |> When (CancelRequest ("jdoe", request.RequestId))
+      |> When (CancelRequest ("jdoe", reqOctober1.RequestId))
       |> Then (Error "Request cannot be canceled") "The request should have been canceled by the employee."
     }
   ]
-
-
-
 
 
 [<Tests>]
 let cancellationClaimsTests =
   testList "Canellation claims tests" [
     test "An employee claims a cancellation (request in the past)" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 7, 1); HalfDay = AM }
-        End = { Date = DateTime(2019, 7, 31); HalfDay = PM } }
-
-      Given [ RequestCreated request ; RequestValidated request ]
+      Given [ RequestCreated reqJanuaryFull ; RequestValidated reqJanuaryFull ]
       |> ConnectedAs (Employee "jdoe")
       |> WithToday (DateTime(2019, 12, 12))
-      |> When (CancelRequest ("jdoe", request.RequestId))
-      |> Then (Ok [CancellationClaimed request]) "The request should have been in pending cancellation."
+      |> When (CancelRequest ("jdoe", reqJanuaryFull.RequestId))
+      |> Then (Ok [CancellationClaimed reqJanuaryFull]) "The request should have been in pending cancellation."
     }
 
     test "The manager reject a cancellation claim" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 7, 1); HalfDay = AM }
-        End = { Date = DateTime(2019, 7, 31); HalfDay = PM } }
-
-      Given [ RequestCreated request ; RequestValidated request ; CancellationClaimed request]
+      Given [ RequestCreated reqJanuaryFull ; RequestValidated reqJanuaryFull ; CancellationClaimed reqJanuaryFull]
       |> ConnectedAs Manager
       |> WithToday (DateTime(2019, 12, 12))
-      |> When (RejectCancellationClaim ("jdoe", request.RequestId))
-      |> Then (Ok [CancellationRejected request]) "The cancellation claim should have been rejected."
+      |> When (RejectCancellationClaim ("jdoe", reqJanuaryFull.RequestId))
+      |> Then (Ok [CancellationRejected reqJanuaryFull]) "The cancellation claim should have been rejected."
     }
 
     test "The manager accepts a cancellation claim" {
-      let request = {
-        UserId = "jdoe"
-        RequestId = Guid.NewGuid()
-        Start = { Date = DateTime(2019, 7, 1); HalfDay = AM }
-        End = { Date = DateTime(2019, 7, 31); HalfDay = PM } }
-
-      Given [ RequestCreated request ; RequestValidated request ; CancellationClaimed request]
+      Given [ RequestCreated reqJanuaryFull ; RequestValidated reqJanuaryFull ; CancellationClaimed reqJanuaryFull]
       |> ConnectedAs Manager
       |> WithToday (DateTime(2019, 12, 12))
-      |> When (CancelRequest ("jdoe", request.RequestId))
-      |> Then (Ok [RequestCanceled request]) "The cancellation claim should have been accepted."
+      |> When (CancelRequest ("jdoe", reqJanuaryFull.RequestId))
+      |> Then (Ok [RequestCanceled reqJanuaryFull]) "The cancellation claim should have been accepted."
     }
   ]
